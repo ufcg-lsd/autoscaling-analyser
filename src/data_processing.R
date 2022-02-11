5f4e15cd0aa38b36ad104ad93762fbd621cc911d
samarasss <samara.sampaio@ccc.ufcg.edu.br>
2022-02-10 22:37:25 -0300

    Add Processing for Azure data + merge main

--- src/data_processing.R
+++ src/data_processing.R
@@ -16,3 +16,26 @@ processing_data <- function(filename){
   
   return(data)
 }
+
+# Processing for Azure data
+processing_data_azure <- function(filename_vm_table, filename_vm_cpu){
+  # Add colum names
+  vm_info <- read.csv(filename_vm_table)
+  colnames(vm_info) <- c("vm_id", "subscription_id", "deployment_id", "timestamp_vm_created", "timestamp_vm_deleted", 
+                         "max_cpu_vm", "avg_cpu_vm", "p95_max_cpu", "vm_category", "vm_virtual_core_count_bucket",
+                         "vm_memory_bucket")
+  vm_cpu <- read.csv(filename_vm_cpu)
+  colnames(vm_cpu) <- c("timestamp", "vm_id", "min_cpu", "max_cpu", "avg_cpu")
+  
+  # Concatenate the information from the two tables with a left join
+  left_join<-left_join(vm_cpu,vm_info,by="vm_id")
+  
+  # Calculates how much of the instance being used
+  data <- left_join %>% 
+    mutate(Used = vm_virtual_core_count_bucket * avg_cpu / 100) %>%
+    na.omit() %>%
+    group_by(timestamp) %>%
+    summarise(Cores = sum(Used))
+  
+  return(data)
+}
--- src/main.R
+++ src/main.R
@@ -5,13 +5,18 @@ source(here::here("src/data_processing.R"))
 source(here::here("src/auto_scaling_algorithm.R"))
 source(here::here("src/calculate_adi.R"))
 
-# Data files
-args = commandArgs(trailingOnly=TRUE)
-input_data <- read.csv(args[1])
-util_data <- processing_data("data/util_data.csv")
-
 # Config file
 configs <- yaml.load_file("config.yaml")
+
+# Data files
+args = commandArgs(trailingOnly=TRUE)
+#input_data <- read.csv(args[1])
+
+if(configs$data_source == 'azure'){
+  util_data <- processing_data_azure("data/azure/vmtable.csv", "data/azure/sample_azure.csv")
+}else{
+  util_data <- processing_data("data/util_data.csv")
+}
 
 # Test parameters
 initial_allocated_cores <- configs$initial_allocated_cores
@@ -20,12 +25,12 @@ policy_parameters <- data.frame(lower_bound = configs$lower_bound,
                                 step_size = configs$step_size)
 
 # Play around with util data
-#data_with_auto_scaling <- auto_scaling_algorithm(util_data, initial_allocated_cores,
-#                                                 policy_parameters)
+data_with_auto_scaling <- auto_scaling_algorithm(util_data, initial_allocated_cores,
+                                                 policy_parameters)
 
 # Play around with input data
-data_with_auto_scaling <- auto_scaling_algorithm(input_data, initial_allocated_cores,
-                                                  policy_parameters)
+#data_with_auto_scaling <- auto_scaling_algorithm(input_data, initial_allocated_cores,
+#                                                  policy_parameters)
 
 data_with_adi <- calculate_adi(data_with_auto_scaling,
                                policy_parameters[["lower_bound"]],
