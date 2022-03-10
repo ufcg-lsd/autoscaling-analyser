26fcbbd701ecbfe2892ae0d3689d854769411e68
Ítallo Silva <itallo.silva@ccc.ufcg.edu.br>
2022-03-10 11:11:48 -0300

    Adapt the policy to two step sizes

--- src/policies/reactive_static_policy.R
+++ src/policies/reactive_static_policy.R
@@ -2,14 +2,14 @@
 # based on upper and lower bound parameters. The scale happens at a fixed
 # step size.
 reactive_static_policy <- function(system_utilization, upper_bound, lower_bound,
-                                   step_size){
+                                   up_step_size, down_step_size){
   new_cores <- 0
   if(system_utilization > upper_bound){
     # Increase cores by step size if system utilization is higher than upper bound
-    new_cores <- step_size
+    new_cores <- up_step_size
   } else if(system_utilization < lower_bound){
     # Decrease cores by step size if system utilization is lower than upper bound
-    new_cores <- step_size * -1
+    new_cores <- down_step_size * -1
   }
   
   return (new_cores)
