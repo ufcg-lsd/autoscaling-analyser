5f196e3dd71c732b30a7e4e509e54b3abd3eafca
Ítallo Silva <itallo.silva@ccc.ufcg.edu.br>
2022-03-14 08:57:43 -0300

    Updating the config.yaml to multiple policies

--- src/main.R
+++ src/main.R
@@ -6,9 +6,9 @@ source(here::here("src/auto_scaling_algorithm.R"))
 source(here::here("src/calculate_adi.R"))
 
 # Data files
-args = commandArgs(trailingOnly=TRUE)
-input_data <- read.csv(args[1])
-util_data <- processing_data(here::here("itallo/master_data_api_070222.csv"))
+# args = commandArgs(trailingOnly=TRUE)
+# input_data <- read.csv(args[1])
+util_data <- processing_data(here::here("itallo/master_data_api_070222_ds-env291-stable.csv"))
 
 # Config file
 configs <- yaml.load_file("config.yaml")
@@ -16,17 +16,14 @@ configs <- yaml.load_file("config.yaml")
 # Test parameters
 initial_allocated_cores <- as.integer(util_data[1, "Allocated"])
 util_data <- util_data[2:nrow(util_data),]
-policy_parameters <- data.frame(lower_bound = configs$lower_bound,
-                                upper_bound = configs$upper_bound,
-                                up_step_size = configs$up_step_size,
-                                down_step_size = configs$down_step_size,
-                                min_cap = configs$min_cap,
-                                max_cap = configs$max_cap)
+policy_parameters <- configs$policies[[configs$policies$to_use]]
+source(here::here(policy_parameters$src))
+policy_parameters$fun <- get(policy_parameters$fun)
 
 # Play around with util data
 data_with_auto_scaling <- auto_scaling_algorithm(util_data, initial_allocated_cores,
                                                 policy_parameters)
-readr::write_csv(data_with_auto_scaling, "itallo/output_master_data_api_070222.csv")
+readr::write_csv(data_with_auto_scaling, "itallo/output_master_data_api_070222_ds-env291-stable.csv")
 
 # Play around with input data
 # data_with_auto_scaling <- auto_scaling_algorithm(input_data, initial_allocated_cores,
