be0ee943b8f5adbacc748c7db1ad929c8e9443cc
Samara Sonale Santos Sampaio <samara@cloud-data-analysis.lsd.ufcg.edu.br>
2022-03-21 23:50:27 +0000

    Run the flow for the adi calculation with the azure data

--- src/data_processing.R
+++ src/data_processing.R
@@ -1,3 +1,5 @@
+library(sjmisc)
+
 processing_data <- function(filename){
   raw_data <- read.csv(filename)
   data <- raw_data %>% 
@@ -17,24 +19,39 @@ processing_data <- function(filename){
   return(data)
 }
 
+# Aggregates files related to VM usage
+aggregate_data_azure <- function(){
+  files_workload <- list.files(path="data/azure/vm_cpu_readings_files", pattern="*.csv", full.names=TRUE, recursive=FALSE)
+  for (file in files_workload){
+    if(str_contains(file, "vm_cpu_readings-file")){
+      print(file)
+      csv_to_add <- read.csv(file)
+      colnames(csv_to_add) <- c("timestamp", "vm_id", "min_cpu", "max_cpu", "avg_cpu")
+      write.table(csv_to_add, file = ("data/azure/vm_cpu_readings_files/aggregate.csv"), sep = ",",
+                  append = TRUE, quote = FALSE,
+                  col.names = !file.exists(("data/azure/vm_cpu_readings_files/aggregate.csv")), row.names = FALSE)
+    }
+  }
+}
+
 # Processing for Azure data
 processing_data_azure <- function(filename_vm_table, filename_vm_cpu){
-  # Add colum names
+  # Add column names
   vm_info <- read.csv(filename_vm_table)
   colnames(vm_info) <- c("vm_id", "subscription_id", "deployment_id", "timestamp_vm_created", "timestamp_vm_deleted", 
                          "max_cpu_vm", "avg_cpu_vm", "p95_max_cpu", "vm_category", "vm_virtual_core_count_bucket",
                          "vm_memory_bucket")
   vm_cpu <- read.csv(filename_vm_cpu)
-  colnames(vm_cpu) <- c("timestamp", "vm_id", "min_cpu", "max_cpu", "avg_cpu")
+  #colnames(vm_cpu) <- c("timestamp", "vm_id", "min_cpu", "max_cpu", "avg_cpu")
   
   # Concatenate the information from the two tables with a left join
   left_join<-left_join(vm_cpu,vm_info,by="vm_id")
-  
+
   # Calculates how much of the instance being used
   data <- left_join %>% 
-    mutate(Used = vm_virtual_core_count_bucket * avg_cpu / 100) %>%
+    mutate(Used = as.numeric(vm_virtual_core_count_bucket) * as.numeric(avg_cpu) / 100) %>%
     na.omit() %>%
-    group_by(timestamp) %>%
+    group_by(timestamp, subscription_id) %>%
     summarise(Cores = sum(Used))
   
   return(data)
--- src/main.R
+++ src/main.R
@@ -1,9 +1,11 @@
 library(dplyr)
 library(yaml)
+library(readr)
 
 source(here::here("src/data_processing.R"))
 source(here::here("src/auto_scaling_algorithm.R"))
 source(here::here("src/calculate_adi.R"))
+source(here::here("src/split_data.R"))
 
 # Config file
 configs <- yaml.load_file("config.yaml")
@@ -12,11 +14,6 @@ configs <- yaml.load_file("config.yaml")
 args = commandArgs(trailingOnly=TRUE)
 #input_data <- read.csv(args[1])
 
-if(configs$data_source == 'azure'){
-  util_data <- processing_data_azure("data/azure/vmtable.csv", "data/azure/sample_azure.csv")
-}else{
-  util_data <- processing_data("data/util_data.csv")
-}
 
 # Test parameters
 initial_allocated_cores <- configs$initial_allocated_cores
@@ -24,17 +21,49 @@ policy_parameters <- data.frame(lower_bound = configs$lower_bound,
                                 upper_bound = configs$upper_bound,
                                 step_size = configs$step_size)
 
-# Play around with util data
-data_with_auto_scaling <- auto_scaling_algorithm(util_data, initial_allocated_cores,
-                                                 policy_parameters)
 
-# Play around with input data
-#data_with_auto_scaling <- auto_scaling_algorithm(input_data, initial_allocated_cores,
-#                                                  policy_parameters)
+if(configs$data_source == 'azure'){
+  aggregate_data_azure()
+  util_data <- processing_data_azure("data/azure/vm_cpu_readings_files/vmtable.csv", "data/azure/vm_cpu_readings_files/aggregate.csv")
+  split_data_by_application_azure(util_data)
+  
+  files <- list.files(path="data/azure/app", pattern="*.csv", full.names=TRUE, recursive=FALSE)
+  
+  for(file in files){
+    ifelse(!dir.exists(file.path("data/azure", "output")), dir.create(file.path("data/azure", "output")), FALSE)
+    util_data <- read_csv(file)
+    app <- util_data$subscription_id
+    
+    data_with_auto_scaling <- auto_scaling_algorithm(util_data, initial_allocated_cores,
+                                                     policy_parameters)
+    data_with_adi <- calculate_adi(data_with_auto_scaling,
+                                   policy_parameters[["lower_bound"]],
+                                   policy_parameters[["upper_bound"]])
+    
+    row_to_add <- data.frame(app, sum(data_with_adi["ADI"]))
+    colnames(row_to_add) <- c("Subscription_id", "ADI")
+    write.table(row_to_add, file = "data/azure/output/metrics.csv", sep=',',
+                append = TRUE, quote = FALSE,
+                col.names = !file.exists("data/azure/output/metrics.csv"), row.names = FALSE)
+  }
+}else{
+  util_data <- processing_data("data/util_data.csv")
+  
+  # Play around with util data
+  data_with_auto_scaling <- auto_scaling_algorithm(util_data, initial_allocated_cores,
+                                                   policy_parameters)
+  # Play around with input data
+  #data_with_auto_scaling <- auto_scaling_algorithm(input_data, initial_allocated_cores,
+  #                                                  policy_parameters)
+  
+  data_with_adi <- calculate_adi(data_with_auto_scaling,
+                                 policy_parameters[["lower_bound"]],
+                                 policy_parameters[["upper_bound"]])
+  sum(data_with_adi["ADI"])
+  
+  
+}
 
-data_with_adi <- calculate_adi(data_with_auto_scaling,
-                               policy_parameters[["lower_bound"]],
-                               policy_parameters[["upper_bound"]])
-sum(data_with_adi["ADI"])
+
 
 
