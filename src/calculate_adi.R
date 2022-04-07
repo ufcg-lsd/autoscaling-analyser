f1852be8a49f3f9adb5c299ccac961bf7fce6c10
Ítallo Silva <itallo.silva@ccc.ufcg.edu.br>
2022-04-07 16:48:07 -0300

    Adding ADI Sign column to data

--- src/calculate_adi.R
+++ src/calculate_adi.R
@@ -3,6 +3,7 @@ calculate_adi <- function(data, lower_bound, upper_bound){
   for(row in 1:nrow(data)) {
     if(data[row,"SystemUtilization"] > upper_bound){
       adi <- data[row,"SystemUtilization"] - upper_bound
+      adi_sign <- 1
       # Take in consideration exceeded cores
       if (data[row, "ExceededCores"] > 0) {
         over_utilization <- data[row, "ExceededCores"] /
@@ -11,22 +12,29 @@ calculate_adi <- function(data, lower_bound, upper_bound){
       }
       
     }else if(data[row,"SystemUtilization"] < lower_bound){
-      adi <- lower_bound - data[row,"SystemUtilization"] 
+      adi <- lower_bound - data[row,"SystemUtilization"]
+      adi_sign <- -1
     
     } else{
       adi <- 0
+      adi_sign <- 0
     }
     
     if(data[row,"SystemRealUtilization"] > upper_bound){
       real_adi <- data[row,"SystemRealUtilization"] - upper_bound
+      real_adi_sign <- 1
     }else if(data[row,"SystemRealUtilization"] < lower_bound){
-      real_adi <- lower_bound - data[row,"SystemRealUtilization"] 
+      real_adi <- lower_bound - data[row,"SystemRealUtilization"]
+      real_adi_sign <- -1
     } else{
       real_adi <- 0
+      real_adi_sign <- 0
     }
     
     data[row, "ADI"] <- adi
+    data[row, "ADISign"] <- adi_sign
     data[row, "RealADI"] <- real_adi
+    data[row, "RealADISign"] <- real_adi_sign
   }
   
   return(data)
