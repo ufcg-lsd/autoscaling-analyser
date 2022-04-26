7d78f1551b6bb2659830571ce7ad397c20f096a9
Kilian Macedo Melcher <kilian.melcher@gmail.com>
2022-04-26 00:12:41 +0000

    [Target scaling] Rollback on cooldown and refactor adjustment

--- src/auto_scaling_algorithm.R
+++ src/auto_scaling_algorithm.R
@@ -1,27 +1,12 @@
 
 auto_scaling_algorithm <- function(data, initial_allocated_cores,
                                    policy_parameters, time_parameters){
-  # Get scaling cooldown
-  if (is.null(policy_parameters$scale_up_cooldown) ||
-      is.null(policy_parameters$scale_down_cooldown)) {
-    # Same cooldown for both scale up and down
-    scale_up_cooldown <- time_parameters$cooldown
-    scale_down_cooldown <- time_parameters$cooldown
-  } else {
-    # Different cooldown for each scale type
-    scale_up_cooldown <- policy_parameters$scale_up_cooldown
-    scale_down_cooldown <- policy_parameters$scale_down_cooldown
-  }
-  
   cores_allocated <- initial_allocated_cores
   
-  # Cooldown scale up counters
-  cooldown_up_start <- -1
-  cooldown_up_countdown <- 0
-  # Cooldown scale down counters
-  cooldown_down_start <- -1
-  cooldown_down_countdown <- 0
-  
+  # Cooldown counters
+  cooldown_start <- -1
+  cooldown_countdown <- 0
+
   adding_time <- c() # Timestaps when there is a scaling operation
   adding_cores <- c()
   
@@ -34,7 +19,7 @@ auto_scaling_algorithm <- function(data, initial_allocated_cores,
       new_cores <- adding_cores[1]
       adding_time <- adding_time[-1]
       adding_cores <- adding_cores[-1]
-      
+
       cores_allocated <- 
         min(
           max(cores_allocated + new_cores, policy_parameters[["min_cap"]]),
@@ -57,28 +42,21 @@ auto_scaling_algorithm <- function(data, initial_allocated_cores,
     data[row, "AllocatedCores"] <- cores_allocated
     
     # Checks if the boot of an instance has ended and the cooldown should start
-    if (cooldown_up_start == current_time) {
+    if (cooldown_start == current_time) {
       # Scale up countdown reset
-      cooldown_up_countdown <- scale_up_cooldown
-      cooldown_up_start <- -1
+      cooldown_countdown <- time_parameters$cooldown
+      cooldown_start <- -1
     }
-    
-    if (cooldown_down_start == current_time) {
-      # Scale down countdown reset
-      cooldown_down_countdown <- scale_down_cooldown
-      cooldown_down_start <- -1
-    }
-    
-    # TODO Make it work for cooldown_down_countdown
-    if (cooldown_up_countdown == 0) {
+
+    if (cooldown_countdown == 0) {
+
       # Scale operation based on policy 
       cores <- policy_parameters$func(
         system_utilization,
         policy_parameters,
         history = data["SystemUtilization"],
         current = row,
-        allocated = cores_allocated,
-        demanded_cores = data[[row,"Cores"]]
+        allocated = cores_allocated
       )
       
       if (cores < 0) {
@@ -93,7 +71,7 @@ auto_scaling_algorithm <- function(data, initial_allocated_cores,
           )
         
         # The cooldown period will start just after the removal.
-        cooldown_up_countdown <- scale_up_cooldown
+        cooldown_countdown <- time_parameters$cooldown
   
       } else if (cores > 0) {
         
@@ -108,18 +86,14 @@ auto_scaling_algorithm <- function(data, initial_allocated_cores,
         ))
         
         cooldown_start <- current_time + time_parameters$boot * 60
-        cooldown_up_start <- cooldown_start
-        # TODO Is it necessary to differentiate the type of cooldown here?
         adding_time <- c(adding_time, cooldown_start + warmup_time * 60)
         adding_cores <- c(adding_cores, cores)
-        
       }
       
     }
     
-    cooldown_up_countdown <- max(0, cooldown_up_countdown - 1)
-    cooldown_down_countdown <- max(0, cooldown_down_countdown - 1)
-    
+    cooldown_countdown <- max(0, cooldown_countdown - 1)
+
     data[row, "NewCores"] <- new_cores
   }
   
