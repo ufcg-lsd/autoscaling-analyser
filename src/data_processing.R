8df253884f6576731006b5d415406e5f9c53a97d
Ítallo Silva <itallo.silva@ccc.ufcg.edu.br>
2022-05-06 10:00:04 -0300

    Include function to prepare input as using the max metric

--- src/data_processing.R
+++ src/data_processing.R
@@ -14,8 +14,29 @@ process_util_data <- function(filename){
     mutate(Used = InstanceCapacity * Average / 100) %>%
     group_by(timestamp) %>%
     summarise(Cores = sum(Used),
+              CoresMax = max(Used) * n(),
               RealAllocatedCores = sum(InstanceCapacity),
               SystemRealUtilization = min((Cores/RealAllocatedCores) * 100, 100))
 
+  return(data)
+}
+
+process_util_data_max <- function(filename){
+  raw_data <- read.csv(filename)
+  
+  cpu_info <- read_csv(here::here("data/cpu_info.csv"), 
+                       col_types = cols_only(
+                         InstanceType = col_character(),
+                         vCPUs = col_integer()
+                       )) %>% rename(InstanceCapacity = vCPUs)
+  
+  data <- raw_data %>% 
+    left_join(cpu_info, by = "InstanceType") %>%
+    mutate(Used = InstanceCapacity * Average / 100) %>%
+    group_by(timestamp) %>%
+    summarise(Cores = max(Used) * n(),
+              RealAllocatedCores = sum(InstanceCapacity),
+              SystemRealUtilization = min((Cores/RealAllocatedCores) * 100, 100))
+  
   return(data)
 }
