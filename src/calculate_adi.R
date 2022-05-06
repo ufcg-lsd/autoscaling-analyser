e4b1e5053e98c9a47bb2373a53cbc4c23c15e18c
fabiomorais <fabio.jorge@gmail.com>
2022-05-06 11:46:53 -0300

    Refactoring ADI computation

--- src/calculate_adi.R
+++ src/calculate_adi.R
@@ -1,41 +1,31 @@
-calculate_adi <- function(data, lower_bound, upper_bound){
+calculate_row_adi <- function(util, L, U){
   
-  for(row in 1:nrow(data)) {
-    if(data[row,"SystemUtilization"] > upper_bound){
-      adi <- data[row,"SystemUtilization"] - upper_bound
-      adi_sign <- 1
-      # Take in consideration exceeded cores
-      if (data[row, "ExceededCores"] > 0) {
-        over_utilization <- data[row, "ExceededCores"] /
-                            data[row, "AllocatedCores"] * 100
-        data[row, "OverUtilization"] <- over_utilization
-      }
-      
-    }else if(data[row,"SystemUtilization"] < lower_bound){
-      adi <- lower_bound - data[row,"SystemUtilization"]
-      adi_sign <- -1
-    
-    } else{
-      adi <- 0
-      adi_sign <- 0
-    }
-    
-    if(data[row,"SystemRealUtilization"] > upper_bound){
-      real_adi <- data[row,"SystemRealUtilization"] - upper_bound
-      real_adi_sign <- 1
-    }else if(data[row,"SystemRealUtilization"] < lower_bound){
-      real_adi <- lower_bound - data[row,"SystemRealUtilization"]
-      real_adi_sign <- -1
-    } else{
-      real_adi <- 0
-      real_adi_sign <- 0
-    }
-    
-    data[row, "ADI"] <- adi
-    data[row, "ADISign"] <- adi_sign
-    data[row, "RealADI"] <- real_adi
-    data[row, "RealADISign"] <- real_adi_sign
+  if(util > U) {
+    return(util - U)
+  } else if (util < L) {
+    return(util - L)
+  } else {
+    return(0)
   }
+}
+
+calculate_adi <- function(data, lower_bound, upper_bound){
+
+  # calculating ADI
+  data$ADI = data$SystemUtilization %>% map_dbl(calculate_row_adi, L=lower_bound, U=upper_bound)
+  data$RealADI = data$SystemRealUtilization %>% map_dbl(calculate_row_adi, L=lower_bound, U=upper_bound)
   
+  # calculating ADI sign
+  data <- data %>%
+    mutate(ADISign = ADI / abs(ADI),
+           RealADISign = RealADI / abs(RealADI)) %>%
+    mutate(ADI = abs(ADI),
+           RealADI = abs(RealADI))
+
+  # calculating overutilization
+  data <- data %>% 
+    mutate(OverUtilization = ifelse(ExceededCores > 0, 
+                                    ExceededCores / AllocatedCores * 100,0)
+           )
   return(data)
 }
