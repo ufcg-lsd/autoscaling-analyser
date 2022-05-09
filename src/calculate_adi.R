2a09ee6a40f421b705e1821fc51d32d421e1b09f
Kilian Macedo Melcher <kilian.melcher@gmail.com>
2022-05-09 23:32:56 +0000

    Use sign function instead of dividing by absolute number to avoid NaN Error

--- src/calculate_adi.R
+++ src/calculate_adi.R
@@ -1,5 +1,5 @@
 calculate_row_adi <- function(util, L, U){
-  
+
   if(util > U) {
     return(util - U)
   } else if (util < L) {
@@ -14,17 +14,17 @@ calculate_adi <- function(data, lower_bound, upper_bound){
   # calculating ADI
   data$ADI = data$SystemUtilization %>% map_dbl(calculate_row_adi, L=lower_bound, U=upper_bound)
   data$RealADI = data$SystemRealUtilization %>% map_dbl(calculate_row_adi, L=lower_bound, U=upper_bound)
-  
+
   # calculating ADI sign
   data <- data %>%
-    mutate(ADISign = ADI / abs(ADI),
-           RealADISign = RealADI / abs(RealADI)) %>%
+    mutate(ADISign = sign(ADI),
+           RealADISign = sign(RealADI)) %>%
     mutate(ADI = abs(ADI),
            RealADI = abs(RealADI))
 
   # calculating overutilization
-  data <- data %>% 
-    mutate(OverUtilization = ifelse(ExceededCores > 0, 
+  data <- data %>%
+    mutate(OverUtilization = ifelse(ExceededCores > 0,
                                     ExceededCores / AllocatedCores * 100,0)
            )
   return(data)
