36de9a64917b64e90f83774db5c808b46dc67155
marcellasiqueira <marcella.almeida@ccc.ufcg.edu.br>
2022-07-07 16:43:27 -0300

    implementing predictive function

--- src/policies/predictive_scaling_policy.R
+++ src/policies/predictive_scaling_policy.R
@@ -7,30 +7,25 @@ predictive_scaling_policy <- function(system_utilization, policy_parameters, ...
   # Arguments parameters
   arguments <- list(...)
   allocated <- arguments$allocated
-  used_cores <- arguments$used_cores
-  cores <- arguments$cores
+  used_cores <- arguments$used_cores[1:arguments$current]
+  cooldown_countdown <- arguments$cooldown
   
-  horizon = 12
-  prediction = forecast(auto.arima(data[start:end, 2]), h=horizon)
-  new_cores <- 0
-  # if(system_utilization > target | system_utilization < lower_threshold){
-  #   # Adjust amount of cores if it's outside of boundaries
-  #   new_cores <- adjustment
-  # }
-  # 
-  # if (new_cores < 0) {
-  #   if (cooldown_countdown$up != 0 || cooldown_countdown$down != 0)
-  #     new_cores <- 0
-  # } else if (new_cores > 0) {
-  #   if (cooldown_countdown$down != 0) {
-  #     cooldown_countdown$down <- 0
-  #   } else if (cooldown_countdown$up != 0) {
-  #     if (new_cores > last_addition) {
-  #       new_cores <- new_cores - last_addition
-  #       cooldown_countdown$up <- 0
-  #     } else new_cores <- 0
-  #   }
-  # }
+  #Policy parameters
+  train_file <- read_csv(policy_parameters$train_file)
+  vm_cores <- policy_parameters$vm_cores
+  train_file <- bind_rows(train_file, used_cores)
+  
+  horizon <- 12
+  prediction_frame <- 21600
+  start <- min(1, arguments$current - prediction_frame + 1)
+  end <- arguments$current
+  
+  prediction <- forecast(auto.arima(train_file[start:end]), h=horizon)
+  adjustment <- prediction$mean[length(prediction$mean)] - allocated
+  new_cores <- ceiling(adjustment / vm_cores) * vm_cores
+  
+  if (cooldown_countdown$up != 0 || cooldown_countdown$down != 0)
+    new_cores <- 0
   
   return (new_cores)
 }
