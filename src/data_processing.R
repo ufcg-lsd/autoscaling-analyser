c5d1f625f6f070a57a41fa350c18187215813ec8
Kilian Macedo Melcher <kilian.melcher@gmail.com>
2022-07-25 12:54:59 +0000

    Plot real and simulation utiization together

--- src/data_processing.R
+++ src/data_processing.R
@@ -14,7 +14,6 @@ process_util_data <- function(filename){
     mutate(Used = InstanceCapacity * Average / 100) %>%
     group_by(timestamp) %>%
     summarise(Cores = sum(Used),
-              CoresMax = max(Used) * n(),
               RealAllocatedCores = sum(InstanceCapacity),
               RealSystemUtilization = min((Cores/RealAllocatedCores) * 100, 100))
 
@@ -37,6 +36,35 @@ process_util_data_max <- function(filename){
     summarise(Cores = max(Used) * n(),
               RealAllocatedCores = sum(InstanceCapacity),
               SystemRealUtilization = min((Cores/RealAllocatedCores) * 100, 100))
+  
+  return(data)
+}
+
+process_experiment_data <- function(processed_data, version, day){
+  # Read
+  raw_data <- read.csv(filename)
+  cpu_info <- read_csv(here::here("data/cpu_info.csv"),
+                       col_types = cols_only(
+                         InstanceType = col_character(),
+                         vCPUs = col_integer()
+                       )) %>% rename(InstanceCapacity = vCPUs)
+
+  # Process
+  processed_data <- raw_data %>%
+    left_join(cpu_info, by = "InstanceType") %>%
+    mutate(Used = InstanceCapacity * Average / 100) %>%
+    group_by(timestamp, Version) %>%
+    summarise(Cores = sum(Used),
+              RealAllocatedCores = sum(InstanceCapacity),
+              RealSystemUtilization = min((Cores/RealAllocatedCores) * 100, 100))
+
+  # Filter
+  data_filtered_by_version <- filter(processed_data, Version == version)
+  data_split_by_days <- data_filtered_by_version %>%
+    mutate(date = as.POSIXct(timestamp, origin="1970-01-01")) %>% 
+    mutate(timestamp_day = timestamp %/% 86400) %>%
+    group_by(timestamp_day) %>% group_split()
+  data <- data_split_by_days[[day]]
   
   return(data)
 }
--- src/plot.R
+++ src/plot.R
@@ -1,9 +1,13 @@
 library(ggplot2)
 
-plot_simulation <- function(data, policy_parameters, configs) {
-  policy_name = configs$policies$use
+plot_util <- function(data, policy_parameters, policy_name, filepath) {
+  version <- 55
+  date <- 15
+  title <- paste(policy_name, "Simulation from version", version,  "at", date)
+  
   plot <- data %>% ggplot(
     aes(x = timestamp - min(timestamp), y = SystemUtilization)) +
+    ggtitle(title) +
     geom_line() +
     theme_minimal() +
     labs(y = "CPUtilization (%)", x = "Time (min)")
@@ -17,25 +21,65 @@ plot_simulation <- function(data, policy_parameters, configs) {
       geom_text(aes(x = 70, y = upper_bound+2, label = "Upper Bound"), size = 3, color = "red") +
       geom_hline(yintercept = lower_bound, color = "blue") +
       geom_text(aes(x = 70, y = lower_bound-2, label = "Lower Bound"), size = 3, color = "blue")
-  
+    
   } else if (policy_name == "target_tracking") {
     target <- policy_parameters$target_value
     
     plot <- plot +
       geom_hline(yintercept = target, color = "red") +
       geom_text(aes(x = 120, y = target+2, label = "Target threshold"), size = 3, color = "red")
-  
+    
   }
-
-  utilization_plot_filepath <- paste("output/", policy_name, "_utilization.pdf", sep='')
-  ggplot2::ggsave(utilization_plot_filepath, width = 7, height = 4)
   
-  data %>% ggplot(aes(x = timestamp - min(timestamp), y = AllocatedCores)) +
+  # Real plot
+  plot <- plot + geom_line(aes(y = RealSystemUtilization), color = "green")
+  
+  ggplot2::ggsave(filepath, width = 7, height = 4)
+}
+
+plot_simulation <- function(data, policy_parameters, configs) {
+  policy_name = configs$policies$use
+  utilization_plot_filepath <- configs$plot_utilization_output_file
+  cores_plot_filepath <- configs$plot_cores_output_file
+
+  plot_util(data, policy_parameters, policy_name, utilization_plot_filepath)
+  
+  # TODO plot cores
+  # data %>% ggplot(aes(x = timestamp - min(timestamp), y = AllocatedCores)) +
+  #   geom_line() +
+  #   theme_minimal() +
+  #   labs(y = "Cores", x = "Tempo (min)")
+  # 
+  # ggplot2::ggsave(cores_plot_filepath, width = 7, height = 4)
+}
+
+plot_real <- function(data, upper_bound, lower_bound) {
+  version <- 55
+  day <- 15
+  title <- paste("Version", version, "from", day)
+
+  # utilization_plot <- data %>% ggplot(
+  #   aes(x = timestamp - min(timestamp), y = RealSystemUtilization)) +
+  #   ggtitle(title) +
+  #   geom_line() +
+  #   theme_minimal() +
+  #   labs(y = "CPUtilization (%)", x = "Time (min)") +
+  #   geom_hline(yintercept = upper_bound, color = "red") +
+  #   geom_text(aes(x = 80, y = upper_bound+2, label = "Upper Bound"), size = 2, color = "red") +
+  #   geom_hline(yintercept = lower_bound, color = "blue") +
+  #   geom_text(aes(x = 80, y = lower_bound-2, label = "Lower Bound"), size = 2, color = "blue")
+  # 
+  # plot_filepath <- paste("output/experiments/utilization/", version, "_", day, ".pdf", sep='')
+  # ggplot2::ggsave(plot_filepath, width = 7, height = 4)
+
+  cores_plot <- data %>% ggplot(
+    aes(x = timestamp - min(timestamp) , y = RealAllocatedCores)) +
+    ggtitle(title) +
     geom_line() +
     theme_minimal() +
-    labs(y = "Cores", x = "Tempo (min)")
-  
-  cores_plot_filepath <- paste("output/", policy_name, "_cores.pdf", sep='')
-  ggplot2::ggsave(cores_plot_filepath, width = 7, height = 4)
-  
+    labs(x = "Time (min)", y = "Cores")
+
+  plot_filepath <- paste("output/experiments/cores/", version, "_", day, ".pdf", sep='')
+  ggplot2::ggsave(plot_filepath, width = 7, height = 4)
+
 }
